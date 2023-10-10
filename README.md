867be795d5a43ab342bd860b01463547867fb113
Simon Sawert <simon@sawert.se>
2023-10-10 22:58:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# `amage` - Generate AMA cards
+
+Generate your own categories for Alla mot alla!
+
+## Setup
+
+* Initially following
+  [this](https://blog.logrocket.com/how-to-set-up-node-typescript-express/)
---
+++ index.ts
@@ -0,0 +1,20 @@
+"use strict";
+
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 8080;
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
