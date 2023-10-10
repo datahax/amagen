1508692bafff78651146d6e47725ec85862b20f5
Simon Sawert <simon@sawert.se>
2023-10-10 23:41:35 +0200

    Rewrite text split, add eslint

---
+++ public/image.js
@@ -0,0 +1,58 @@
+function splitToLines(ctx, text, maxWidth) {
+  var words = text.split(" ");
+  var lines = [];
+  var currentLine = words[0];
+
+  for (var i = 1; i < words.length; i++) {
+    var word = words[i];
+    var width = ctx.measureText(currentLine + " " + word).width;
+    if (width < maxWidth) {
+      currentLine += " " + word;
+    } else {
+      lines.push(currentLine);
+      currentLine = word;
+    }
+  }
+
+  lines.push(currentLine);
+
+  return lines;
+}
+
+const imageObj = new Image();
+const canvas = document.getElementById("amaBoard");
+var ctx = canvas.getContext("2d");
+
+var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+gradient.addColorStop("0", "gold");
+gradient.addColorStop("0.5", "white");
+gradient.addColorStop("1.0", "gold");
+
+ctx.textAlign = "center";
+ctx.font = "800 60px Arial";
+ctx.fillStyle = gradient;
+ctx.strokeStyle = gradient;
+ctx.color = "red";
+ctx.shadowBlur = 5;
+ctx.shadowColor = "gold";
+
+var maxWidth = 800;
+var lineHeight = 70;
+var text = "Programledaren Filip Hammar är död. Så här minns Ekot honom.";
+var textLines = splitToLines(ctx, text.toUpperCase(), maxWidth);
+var x = canvas.width / 2 - 90;
+var y = canvas.height / 2;
+
+if (textLines.length > 1) {
+  y -= lineHeight * textLines.length / 2 - lineHeight;
+}
+
+imageObj.src = "canvas.jpg";
+imageObj.onload = function () {
+  ctx.drawImage(imageObj, 0, 0);
+
+  for (var i = 0; i < textLines.length; i++) {
+    ctx.fillText(textLines[i], x, y);
+    y += lineHeight;
+  }
+};
