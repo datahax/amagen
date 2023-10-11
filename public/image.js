a6cd2458bf0c87b0350c7d74ae53b9e8d97fa85e
Simon Sawert <simon@sawert.se>
2023-10-11 23:43:14 +0200

    Add support to update image text

--- public/image.js
+++ public/image.js
@@ -19,40 +19,48 @@ function splitToLines(ctx, text, maxWidth) {
   return lines;
 }
 
-const imageObj = new Image();
-const canvas = document.getElementById("amaBoard");
-var ctx = canvas.getContext("2d");
+function generateImage(text) {
+  const imageObj = new Image();
+  const canvas = document.getElementById("amaBoard");
+  var ctx = canvas.getContext("2d");
 
-var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
-gradient.addColorStop("0", "gold");
-gradient.addColorStop("0.5", "white");
-gradient.addColorStop("1.0", "gold");
+  var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+  gradient.addColorStop("0", "gold");
+  gradient.addColorStop("0.5", "white");
+  gradient.addColorStop("1.0", "gold");
 
-ctx.textAlign = "center";
-ctx.font = "800 60px Arial";
-ctx.fillStyle = gradient;
-ctx.strokeStyle = gradient;
-ctx.color = "red";
-ctx.shadowBlur = 5;
-ctx.shadowColor = "gold";
+  ctx.textAlign = "center";
+  ctx.font = "800 60px Arial";
+  ctx.fillStyle = gradient;
+  ctx.strokeStyle = gradient;
+  ctx.color = "red";
+  ctx.shadowBlur = 5;
+  ctx.shadowColor = "gold";
 
-var maxWidth = 800;
-var lineHeight = 70;
-var text = "Programledaren Filip Hammar är död. Så här minns Ekot honom.";
-var textLines = splitToLines(ctx, text.toUpperCase(), maxWidth);
-var x = canvas.width / 2 - 90;
-var y = canvas.height / 2;
+  var maxWidth = 800;
+  var lineHeight = 70;
+  var textLines = splitToLines(ctx, text.toUpperCase(), maxWidth);
+  var x = canvas.width / 2 - 90;
+  var y = canvas.height / 2;
 
-if (textLines.length > 1) {
-  y -= lineHeight * textLines.length / 2 - lineHeight;
+  if (textLines.length > 1) {
+    y -= (lineHeight * textLines.length) / 2 - lineHeight;
+  }
+
+  imageObj.src = "public/canvas.jpg";
+  imageObj.onload = function () {
+    ctx.drawImage(imageObj, 0, 0);
+
+    for (var i = 0; i < textLines.length; i++) {
+      ctx.fillText(textLines[i], x, y);
+      y += lineHeight;
+    }
+  };
 }
 
-imageObj.src = "public/canvas.jpg";
-imageObj.onload = function () {
-  ctx.drawImage(imageObj, 0, 0);
+const textInput = document.querySelector("#text");
+textInput.addEventListener("input", (event) => {
+  generateImage(event.target.value);
+});
 
-  for (var i = 0; i < textLines.length; i++) {
-    ctx.fillText(textLines[i], x, y);
-    y += lineHeight;
-  }
-};
+generateImage("");
