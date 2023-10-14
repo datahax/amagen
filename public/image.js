e84b3e6c7477838628fdeccf1b73bb13905491dd
Johan Hörnsten <johan@datahax.se>
2023-10-14 15:08:56 +0200

    try new transform method

--- public/image.js
+++ public/image.js
@@ -18,7 +18,7 @@ function splitLongWords(words, maxWidth) {
 
 // Helper function to split text into multiple lines
 function splitToLines(ctx, text, maxWidth) {
-  const maxCharsPerLine = 18;
+  const maxCharsPerLine = 14;
   const words = splitLongWords(text.split(" "), maxCharsPerLine);
   const lines = [];
   let currentLine = words[0];
@@ -48,67 +48,19 @@ function splitToLines(ctx, text, maxWidth) {
   return lines;
 }
 
-// Matrix class for affine transformations
-class Matrix {
-  constructor(points) {
-    if (points.length !== 6) {
-      throw new Error("Matrix initialization requires an array of 6 points.");
-    }
-    this.data = [...points];
-  }
-
-  invert() {
-    const [a, b, c, d, e, f] = this.data;
-    const det = a * d - b * c;
-    if (!det) {
-      throw new Error("Matrix is non-invertible.");
-    }
-    return new Matrix([
-      d / det,
-      -b / det,
-      -c / det,
-      a / det,
-      (c * f - d * e) / det,
-      (b * e - a * f) / det,
-    ]);
-  }
-
-  multiply(matrix) {
-    const [a1, b1, c1, d1, e1, f1] = this.data;
-    const [a2, b2, c2, d2, e2, f2] = matrix.data;
-
-    return new Matrix([
-      a1 * a2 + c1 * b2,
-      b1 * a2 + d1 * b2,
-      a1 * c2 + c1 * d2,
-      b1 * c2 + d1 * d2,
-      a1 * e2 + c1 * f2 + e1,
-      b1 * e2 + d1 * f2 + f1,
-    ]);
-  }
-}
-
-// Helper function to set canvas transformation
-function setTransform(ctx, src, dst) {
-  const a = new Matrix(src);
-  const b = new Matrix(dst);
-  const c = b.multiply(a.invert());
-  ctx.setTransform(c.a, c.b, c.c, c.d, c.e, c.f);
-}
-
 // Main function to generate image with styled text
 function generateImage(text) {
   const imageObj = new Image();
   const canvas = document.getElementById("amaBoard");
   const ctx = canvas.getContext("2d");
 
-  // Create the first offscreen canvas
+  // Create the first offscreen canvas for text rendering
   const offscreenCanvas1 = document.createElement("canvas");
   offscreenCanvas1.width = canvas.width;
   offscreenCanvas1.height = canvas.height;
   const offscreenCtx1 = offscreenCanvas1.getContext("2d");
 
-  // Create the second offscreen canvas
+  // Create the second offscreen canvas for blur effect
   const offscreenCanvas2 = document.createElement("canvas");
   offscreenCanvas2.width = canvas.width;
   offscreenCanvas2.height = canvas.height;
@@ -116,14 +68,16 @@ function generateImage(text) {
 
   // Font setup and alignment
   offscreenCtx1.textAlign = "center";
-  offscreenCtx1.font = "700 51px 'Poppins'";
+  offscreenCtx1.font = "700 80px 'Poppins'";
 
   // Text line splitting
-  const maxWidth = 700;
-  const lineHeight = 70;
+  const maxWidth = 1000;
+  const lineHeight = canvas.height / 8;
+  const debugMode = text.toUpperCase().substr(0, 5) === "DEBUG";
   const textLines = splitToLines(offscreenCtx1, text.toUpperCase(), maxWidth);
-  let x = 479; // Center X of the TV screen
-  let y = 305; // Center Y of the TV screen
+  const resetY = () => canvas.height / 2 - 13;
+  let x = canvas.width / 2;
+  let y = resetY();
 
   if (textLines.length > 1) {
     y -= (lineHeight * textLines.length) / 2 - lineHeight;
@@ -141,7 +95,7 @@ function generateImage(text) {
     }
   }
 
-  // Gradient setup
+  // Text fill setup
   const fillAngle = -50 * (Math.PI / 180);
   const length = Math.sqrt(textWidth * textWidth + textHeight * textHeight);
   const x1 = x + (Math.cos(fillAngle - Math.PI / 2) * length) / 2;
@@ -161,14 +115,39 @@ function generateImage(text) {
   gradient.addColorStop(1, "#bab466");
   offscreenCtx1.fillStyle = gradient;
 
+  // Text stroke setup
+  const strokeAngle = -130 * (Math.PI / 180);
+  const strokeX1 = x + (Math.cos(strokeAngle - Math.PI / 2) * length) / 2;
+  const strokeY1 = y + (Math.sin(strokeAngle - Math.PI / 2) * length) / 2;
+  const strokeX2 = x + (Math.cos(strokeAngle + Math.PI / 2) * length) / 2;
+  const strokeY2 = y + (Math.sin(strokeAngle + Math.PI / 2) * length) / 2;
+
+  const strokeGradient = offscreenCtx1.createLinearGradient(
+    strokeX1,
+    strokeY1,
+    strokeX2,
+    strokeY2,
+  );
+  strokeGradient.addColorStop(0, "#b59514");
+  strokeGradient.addColorStop(0.04, "#c2c496");
+  strokeGradient.addColorStop(0.13, "#9e780b");
+  strokeGradient.addColorStop(0.25, "#cad9dc");
+  strokeGradient.addColorStop(0.36, "#d1e7ff");
+  strokeGradient.addColorStop(0.51, "#a68200");
+  strokeGradient.addColorStop(0.67, "#b0bdaa");
+  strokeGradient.addColorStop(0.88, "#bab466");
+  strokeGradient.addColorStop(1, "#bab466");
+  offscreenCtx1.strokeStyle = strokeGradient;
+  offscreenCtx1.lineWidth = 2;
+
+  offscreenCtx1.shadowColor = "rgba(0, 0, 0, 0.5)";
+  offscreenCtx1.shadowBlur = 1;
+  offscreenCtx1.shadowOffsetX = 0;
+  offscreenCtx1.shadowOffsetY = 2;
+
   imageObj.src = "public/canvas.jpg";
   imageObj.onload = function () {
     ctx.drawImage(imageObj, 0, 0);
-
-    // Set the transformation for the TV screen projection
-    const srcPoints = [112, 90, 847, 96, 126, 560];
-    const dstPoints = [0, 0, canvas.width, 0, 0, canvas.height];
-    setTransform(offscreenCtx1, srcPoints, dstPoints);
 
     // Draw the fill text first
     for (let i = 0; i < textLines.length; i++) {
@@ -177,56 +156,71 @@ function generateImage(text) {
     }
 
     // Reset y for shadowed stroke
-    y = 305; // Center Y of the TV screen
+    y = resetY();
     if (textLines.length > 1) {
       y -= (lineHeight * textLines.length) / 2 - lineHeight;
     } else {
       y += lineHeight / 2;
     }
 
-    const strokeAngle = -130 * (Math.PI / 180);
-    const strokeX1 = x + (Math.cos(strokeAngle - Math.PI / 2) * length) / 2;
-    const strokeY1 = y + (Math.sin(strokeAngle - Math.PI / 2) * length) / 2;
-    const strokeX2 = x + (Math.cos(strokeAngle + Math.PI / 2) * length) / 2;
-    const strokeY2 = y + (Math.sin(strokeAngle + Math.PI / 2) * length) / 2;
-
-    const strokeGradient = offscreenCtx1.createLinearGradient(
-      strokeX1,
-      strokeY1,
-      strokeX2,
-      strokeY2,
-    );
-    strokeGradient.addColorStop(0, "#b59514");
-    strokeGradient.addColorStop(0.04, "#c2c496");
-    strokeGradient.addColorStop(0.13, "#9e780b");
-    strokeGradient.addColorStop(0.25, "#cad9dc");
-    strokeGradient.addColorStop(0.36, "#d1e7ff");
-    strokeGradient.addColorStop(0.51, "#a68200");
-    strokeGradient.addColorStop(0.67, "#b0bdaa");
-    strokeGradient.addColorStop(0.88, "#bab466");
-    strokeGradient.addColorStop(1, "#bab466");
-
     // Draw the shadowed stroke
-    offscreenCtx1.strokeStyle = strokeGradient;
-
-    offscreenCtx1.lineWidth = 2;
-    offscreenCtx1.shadowColor = "rgba(0, 0, 0, 0.5)";
-    offscreenCtx1.shadowBlur = 1;
-    offscreenCtx1.shadowOffsetX = 0;
-    offscreenCtx1.shadowOffsetY = 2;
     for (let i = 0; i < textLines.length; i++) {
       offscreenCtx1.strokeText(textLines[i], x, y);
       y += lineHeight;
     }
 
-    // Draw the offscreenCanvas1 onto offscreenCanvas2 with transformation and blur
+    // Draw the offscreenCanvas1 onto offscreenCanvas2 with blur
     offscreenCtx2.filter = "blur(0.5px)";
-    setTransform(offscreenCtx2, srcPoints, dstPoints);
     offscreenCtx2.drawImage(offscreenCanvas1, 0, 0);
     offscreenCtx2.filter = "none"; // Reset the filter
 
-    // Draw the offscreen canvas onto the main canvas
+    // Draw the debugging grid and background over the blurred content
+    const rowHeight = offscreenCanvas2.height / 8;
+    for (let row = rowHeight; row < offscreenCanvas2.height; row += rowHeight) {
+      offscreenCtx2.moveTo(0, row);
+      offscreenCtx2.lineTo(offscreenCanvas2.width, row);
+    }
+
+    const colWidth = offscreenCanvas2.width / 12;
+    for (let col = colWidth; col < offscreenCanvas2.width; col += colWidth) {
+      offscreenCtx2.moveTo(col, 0);
+      offscreenCtx2.lineTo(col, offscreenCanvas2.height);
+    }
+
+    offscreenCtx2.strokeStyle = "red";
+    offscreenCtx2.lineWidth = 0.5;
+    if (debugMode) offscreenCtx2.stroke();
+
+    offscreenCtx2.fillStyle = "rgba(255,0,0,.2)";
+
+    if (debugMode) offscreenCtx2.fillRect(0, 0, canvas.width, canvas.height);
+
+    const topLeft = [113, 92];
+    const topRight = [846, 96];
+    const bottomRight = [842, 540];
+    const bottomLeft = [123, 558];
+
+    const width = Math.sqrt(
+      Math.pow(topRight[0] - topLeft[0], 2) +
+      Math.pow(topRight[1] - topLeft[1], 2)
+    );
+    const height = Math.sqrt(
+      Math.pow(bottomLeft[0] - topLeft[0], 2) +
+      Math.pow(bottomLeft[1] - topLeft[1], 2)
+    );
+
+    // Scaling factors
+    const xScale = width / offscreenCanvas2.width;
+    const yScale = height / offscreenCanvas2.height;
+
+    // Set transformation
+    ctx.setTransform(xScale, 0, 0, yScale, topLeft[0], topLeft[1]);
+
+    // Draw the text onto the main canvas
     ctx.drawImage(offscreenCanvas2, 0, 0);
+
+    // Reset transform
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
   };
 }
 
@@ -237,4 +231,5 @@ textInput.addEventListener("input", (event) => {
 });
 
 // Initial call to the function
-generateImage("");
+const input = document.querySelector("input[type=text]");
+generateImage(input.value);
